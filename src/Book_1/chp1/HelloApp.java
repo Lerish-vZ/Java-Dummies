f683e3b77c5667babd455c256e5542b1c54ab60a
Lerish-vZ <lerishavz@gmail.com>
2024-03-07 10:31:48 +0200

    Update

--- src/Book_1/chp1/HelloApp.java
+++ src/Book_1/chp1/HelloApp.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Book_1.Chpt1;
+package book_1.chp1;
 
 /**
  *
  * @author lerishav
  */
 public class HelloApp {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
