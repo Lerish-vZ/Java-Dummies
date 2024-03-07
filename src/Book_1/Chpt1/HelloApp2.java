97db50407efcbab826e4cae9231ce6be38883105
Lerish-vZ <lerishavz@gmail.com>
2024-03-07 10:28:44 +0200

    first commit

---
+++ src/Book_1/Chpt1/HelloApp2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Book_1.Chpt1;
+
+/**
+ *
+ * @author lerishav
+ */
+public class HelloApp2 {
+    
+}
