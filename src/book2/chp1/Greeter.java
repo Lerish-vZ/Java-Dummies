5a4e8ff246d699a71c843379508469841006bcfe
Lerish-vZ <lerishavz@gmail.com>
2024-03-08 10:23:30 +0200

    update

--- src/book2/chp1/Greeter.java
+++ src/book2/chp1/Greeter.java
@@ -5,7 +5,15 @@
 package book2.chp1;
 
 /* Import statements must appear at the beginning og the class file, before any class declarations.
+
+    You can include as many import statements as are necessary to import all the classes used by your program.
+
+    You can import all the classes in a particular package by listing the package name followed by and asterisk wildcard: import javax.swing.*
+
+    Because many programs use the classes that are contained in the java.lang package, you don't have to import that package. Instead, those classes are automatically
+         available to all programs. The System class is defined in the java.lang package. As a result, you don;t have to provide an import statement to use this class.
     */
+
 import javax.swing.JOptionPane; //used so that you don't have to Fully Qualify the names of the classes when you use them 
 
 /**
