7118a3c14a055a41c2df6684e41095eba214f080
Lerish-vZ <lerishavz@gmail.com>
2024-03-08 10:18:35 +0200

    update

--- src/book2/chp1/Greeter.java
+++ src/book2/chp1/Greeter.java
@@ -4,6 +4,10 @@
  */
 package book2.chp1;
 
+/* Import statements must appear at the beginning og the class file, before any class declarations.
+    */
+import javax.swing.JOptionPane; //used so that you don't have to Fully Qualify the names of the classes when you use them 
+
 /**
  *
  * @author lerishav
@@ -11,6 +15,13 @@ package book2.chp1;
 public class Greeter {
     
     public void sayHello() {
-        System.out.println("Hello, World!");
+       /* System.out.println("Hello, World!"); //prints in console.*/
+       
+       /* Makes a Dialog box pop up with greeting */
+        JOptionPane.showMessageDialog(null, 
+                "Hello, World!", "Greeter", 
+                JOptionPane.INFORMATION_MESSAGE); 
+        /* Shows that you can change the greeter class without having to change
+        code in the main class. One of the advantages of Object-orientated porgram. */
     }
 }
