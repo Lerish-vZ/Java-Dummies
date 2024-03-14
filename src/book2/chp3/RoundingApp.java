392c72bb7adf28666ef94eb481abedb014554555
Lerish-vZ <lerishavz@gmail.com>
2024-03-14 10:36:34 +0200

    update

--- src/book2/chp3/RoundingApp.java
+++ src/book2/chp3/RoundingApp.java
@@ -20,30 +20,30 @@ public class RoundingApp {
         double z = -19.3;
         
         // Returns the integer that is closest to the argument.
-        System.out.println("round(x) = " + Math.round(x));
-        System.out.println("round(y) = " + Math.round(y));
-        System.out.println("round(z) = " + Math.round(z));
+        System.out.println("round(x) = " + Math.round(x)); //= 29
+        System.out.println("round(y) = " + Math.round(y)); //= 94
+        System.out.println("round(z) = " + Math.round(z)); //= -19
         
         System.out.println();
         
         //Returns the smallest double value that is an integer and is greater than or equal to the value of the argument.
-        System.out.println("ceil(x) = " + Math.ceil(x));
-        System.out.println("ceil(y) = " + Math.ceil(y));
-        System.out.println("ceil(z) = " + Math.ceil(z));
+        System.out.println("ceil(x) = " + Math.ceil(x)); //= 30.0
+        System.out.println("ceil(y) = " + Math.ceil(y)); //= 94.0
+        System.out.println("ceil(z) = " + Math.ceil(z)); //= -19.0
         
         System.out.println();
         
         //Returns the largest double value that is an integer and is less than or equal to the value of the argument.
-        System.out.println("floor(x) = " + Math.floor(x));
-        System.out.println("floor(y) = " + Math.floor(y));
-        System.out.println("floor(z) = " + Math.floor(z));
+        System.out.println("floor(x) = " + Math.floor(x)); //= 29.0
+        System.out.println("floor(y) = " + Math.floor(y)); //= 93.0
+        System.out.println("floor(z) = " + Math.floor(z)); //= -20.0
         
         System.out.println();
         
         //Returns the double value that is an integer and is closest to the value of the argument.
-        System.out.println("rint(x) = " + Math.rint(x));
-        System.out.println("rint(y) = " + Math.rint(y));
-        System.out.println("rint(z) = " + Math.rint(z));
+        System.out.println("rint(x) = " + Math.rint(x)); //= 29.0
+        System.out.println("rint(y) = " + Math.rint(y)); //= 94.0
+        System.out.println("rint(z) = " + Math.rint(z)); //= -19.0
     }
 
 }
