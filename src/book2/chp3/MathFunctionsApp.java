432fbe6c112a08927c6579085b03efc20372dd9d
Lerish-vZ <lerishavz@gmail.com>
2024-03-14 09:59:19 +0200

    update

--- src/book2/chp3/MathFunctionsApp.java
+++ src/book2/chp3/MathFunctionsApp.java
@@ -21,18 +21,18 @@ public class MathFunctionsApp {
         double y = 3.0;
         double z = 4.0;
         
-        System.out.println("abs(b) = " + Math.abs(b));
-        System.out.println("cbrt(x) = " + Math.cbrt(x));
-        System.out.println("exp(y) = " + Math.exp(z));
-        System.out.println("hypot(y,z)= " + Math.hypot(y, z));
-        System.out.println("log(y) = " + Math.log(y));
-        System.out.println("log10(y) = " + Math.log10(y));
-        System.out.println("max(a, b) = " + Math.max(a, b));
-        System.out.println("min(a, b) = " + Math.min(a, b));
-        System.out.println("pow(a, c) = " + Math.pow(a, c));
-        System.out.println("random() = " + Math.random());
-        System.out.println("signum(b) = " + Math.signum(b));
-        System.out.println("sqrt(x) = " + Math.sqrt(y));
+        System.out.println("abs(b) = " + Math.abs(b)); //Returns the absolute value of the argument.
+        System.out.println("cbrt(x) = " + Math.cbrt(x)); //Returns the cube root of the argument. 
+        System.out.println("exp(y) = " + Math.exp(z)); //Returns e raised to the power of the argument.
+        System.out.println("hypot(y,z)= " + Math.hypot(y, z)); //Returns the hypotenuse of a right triangle calculated according to the Pythagorean theorem.
+        System.out.println("log(y) = " + Math.log(y)); //Returns the natural logarithm (base e) of the argument. 
+        System.out.println("log10(y) = " + Math.log10(y)); //Returns the base 10 logarithm of the argument.
+        System.out.println("max(a, b) = " + Math.max(a, b)); //Returns the larger of two arguments.
+        System.out.println("min(a, b) = " + Math.min(a, b)); //Returns the smaller of the two arguments.
+        System.out.println("pow(a, c) = " + Math.pow(a, c)); //Returns the value of the first argument raised to the power of the second argument.
+        System.out.println("random() = " + Math.random()); //Returns a random number that's greater than or equal to 0.0 but less than 1.0.
+        System.out.println("signum(b) = " + Math.signum(b)); //Returns a number that represents the sign of the argument: -1.0 if the argument is negative, 0.0 if the argument is zero, and 1.0 if the argument is positive.
+        System.out.println("sqrt(x) = " + Math.sqrt(y)); //Returns the square root of the argument.
     }
 
 }
