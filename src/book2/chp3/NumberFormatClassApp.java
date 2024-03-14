f55632d039f4a346f2eb75c6a27d776ce1550c45
Lerish-vZ <lerishavz@gmail.com>
2024-03-14 11:33:25 +0200

    update

--- src/book2/chp3/NumberFormatClassApp.java
+++ src/book2/chp3/NumberFormatClassApp.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package book2.chp3;
-
+import java.text.NumberFormat;
 /**
  *
  * @author lerishav
@@ -14,8 +14,22 @@ public class NumberFormatClassApp {
     /**
      * @param args the command line arguments
      */
+    static NumberFormat cf = NumberFormat.getCurrencyInstance();
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        printMyAllowance();
+        printCostOfPaintBallGun();
     }
     
+    public static void printMyAllowance(){
+        double myAllowance = 5.00;
+        cf = NumberFormat.getCurrencyInstance();
+        System.out.println("My allowance: " + cf.format(myAllowance));
+    }
+    
+    public static void printCostOfPaintBallGun() {
+        double costOfPaintBallGun = 69.95;
+        cf = NumberFormat.getCurrencyInstance();
+        System.out.println("Cost of Paint Ball Gun: " + cf.format(costOfPaintBallGun));
+    }
 }
