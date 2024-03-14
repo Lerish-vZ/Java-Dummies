e2d6fc18f7d598fbae4209c869cd439060385a48
Lerish-vZ <lerishavz@gmail.com>
2024-03-14 10:06:11 +0200

    update

--- src/book2/chp3/DiceApp.java
+++ src/book2/chp3/DiceApp.java
@@ -16,7 +16,10 @@ public class DiceApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int roll; 
+        String msg = "Here are 100 random rolls of the dice:";
+        
+        
     }
     
 }
