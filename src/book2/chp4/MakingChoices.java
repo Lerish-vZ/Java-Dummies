ecb7972b661adc16699c9420f51aa573cbd5cf70
Lerish-vZ <lerishavz@gmail.com>
2024-03-15 15:08:08 +0200

    update

--- src/book2/chp4/MakingChoices.java
+++ src/book2/chp4/MakingChoices.java
@@ -8,13 +8,26 @@ package book2.chp4;
  *
  * @author lerishav
  */
+/*
+RELATIONAL OPERATORS:
+    ==  Returns true if the expression on the left evalutes to the same value as the expression on the right.
+    !=  Returns true if the expression on the left does not evaluate to the same value as the expression on the right.
+    <   Returns true if the expression on the left evaluates to a value that is less than the value of the expression on the right.
+    <=  Returns true if the expression on the left evaluates to a value that is less than or equal to the expression on the right.
+    >   Returns true if the expression on the left evaluates to a value that is greater then the value the expression on the right.
+    >=  Returns true if the expression on the left evaluates to a value that is greater then or equal to the expression on the right.
+
+SIMPLE IF STATEMENT:    
+    if (boolean-expression)
+        statement
+*/
 public class MakingChoices {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
