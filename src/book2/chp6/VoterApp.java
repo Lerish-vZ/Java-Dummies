e104dc84ed040884ad34d509bb90aab01495b389
Lerish-vZ <lerishavz@gmail.com>
2024-03-25 09:54:03 +0200

    update

--- src/book2/chp6/VoterApp.java
+++ src/book2/chp6/VoterApp.java
@@ -26,6 +26,25 @@ public class VoterApp {
         System.out.print("Enter the error code: ");
         
         int err = sc.nextInt();
+        
+        String msg;
+        if (err == 1) 
+                msg = "Voter marked more than one candidate. \n" + "Ballot rejected.";
+        else if (err == 2)
+            msg = "Box checked and write-in candidate entered. \nBallot rejected.";
+        else if (err == 3)
+            msg = "Entire ballot was blank. \nBallot filled in according to secret plan.";
+        else if (err == 4)
+            msg = "Nothing unusual about the ballot. \nVoter randomly selected for tax audit.";
+        else if (err == 5)
+            msg = "Voter filled in every box. \nBallot counted twice.";
+        else if (err == 6)
+            msg = "Voter drooled in voring machine. \nBegining spin cycle.";
+        else if (err == 7)
+            msg = "Voter lied to pollster after voting. \nVoter's ballot changed to match polling data.";
+        else 
+            msg = "Voter filled out ballot correctly. \nBallot discarded anyway.";
+        System.out.println("msg");
     }
     
 }
