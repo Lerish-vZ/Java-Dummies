bd25f4be944ef1fb782840f0a2da6cf172849e91
Lerish-vZ <lerishavz@gmail.com>
2024-03-25 09:55:58 +0200

    update

---
+++ src/book2/chp6/VoterApp2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book2.chp6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lerishav
+ */
+public class VoterApp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static Scanner sc = new Scanner (System.in);
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the voting machine error code decoder. \n\n" +
+                            "If your voting machine generates an error code, \n" +
+                            "you can use this program to determine the exact \ncause of the error. \n");
+        System.out.print("Enter the error code: ");
+    }
+    
+}
