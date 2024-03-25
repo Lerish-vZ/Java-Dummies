70ee93541f848737c8ec72fe6dbd909a2e39e810
Lerish-vZ <lerishavz@gmail.com>
2024-03-25 09:45:22 +0200

    update

--- src/book2/chp6/VoterApp.java
+++ src/book2/chp6/VoterApp.java
@@ -4,6 +4,8 @@
  */
 package book2.chp6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lerishav
@@ -13,8 +15,17 @@ public class VoterApp {
     /**
      * @param args the command line arguments
      */
+    
+    static Scanner sc = new Scanner (System.in);
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Welcome to the voting machine error code decoder. \n\n" +
+                            "If your voting machine generates an error code, \n" +
+                            "you can use this program to determine the exact \ncause of the error. \n");
+        System.out.print("Enter the error code: ");
+        
+        int err = sc.nextInt();
     }
     
 }
