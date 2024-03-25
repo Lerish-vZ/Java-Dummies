b47ccf2e7d45a9aefa18dbd3713fefa6ab87ffee
Lerish-vZ <lerishavz@gmail.com>
2024-03-25 13:52:44 +0200

    update

--- src/book2/chp6/CarWashStringApp.java
+++ src/book2/chp6/CarWashStringApp.java
@@ -4,17 +4,51 @@
  */
 package book2.chp6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lerishav
  */
+
+//Using a string in a switch statement when the string is received form the user (input)
 public class CarWashStringApp {
 
     /**
      * @param args the command line arguments
      */
+    
+    
+    static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("The car wash application.\n\n");
+        System.out.print("Enter the package code: ");
+        
+        String s = sc.next();
+        
+        String details = "";
+        
+        switch(s.toUpperCase()){
+            case "PRESIDENTIAL":
+                details += "\tNew Car Scent, plus...\n";
+            case "ELITE":
+                details += "\tTire Treatment, plus...\n";
+            case "DELUXE":
+                details += "\tLeather/Vinyl Treatment, plus...\n";
+            case "SUPER":
+                details += "\tWax, plus...\n";
+            case "STANDARD":
+                details += "\tWash, vacuum, and hand dry.\n";
+                break;
+            default:
+                details = "That's not one of the codes.";
+                break;
+        }
+        System.out.println("\nThat package includes: \n");
+        System.out.println(details);
+                    
+        }
     }
     
 }
