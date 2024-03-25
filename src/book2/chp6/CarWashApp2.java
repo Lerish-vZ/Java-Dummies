1bf2f5275bb60eadb3ff8e0def5891986dbe8bea
Lerish-vZ <lerishavz@gmail.com>
2024-03-25 13:01:29 +0200

    update

---
+++ src/book2/chp6/CarWashApp2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book2.chp6;
+
+/**
+ *
+ * @author lerishav
+ */
+//how to prevent fall through in a switch statement
+public class CarWashApp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
