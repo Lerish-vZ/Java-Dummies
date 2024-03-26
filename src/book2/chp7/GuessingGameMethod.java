5ebd1b1e086e30d2b05a65038e5ca97ab988421d
Lerish-vZ <lerishavz@gmail.com>
2024-03-26 11:31:30 +0200

    update

---
+++ src/book2/chp7/GuessingGameMethod.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book2.chp7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lerishav
+ */
+//static methods - means that you can call it without first creating an instance of th eclass in which it's defined.
+
+public class GuessingGameMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static Scanner sc = new Scanner(System.in);
+    static boolean keepPlaying = true;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
