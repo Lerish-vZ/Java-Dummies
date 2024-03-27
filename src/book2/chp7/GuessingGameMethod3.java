eb964854c696161f182b5a290872291bc1b69d65
Lerish-vZ <lerishavz@gmail.com>
2024-03-27 14:12:55 +0200

    update

--- src/book2/chp7/GuessingGameMethod3.java
+++ src/book2/chp7/GuessingGameMethod3.java
@@ -4,6 +4,8 @@
  */
 package book2.chp7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lerishav
@@ -13,8 +15,65 @@ public class GuessingGameMethod3 {
     /**
      * @param args the command line arguments
      */
+    
+    static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Let's play a guessing game!");
+        
+        do {
+            playARound();
+        } while(askForAnotherRound());
+        System.out.println("\nThank you for playing!");
+    }
+    
+    public static void playARound(){
+        boolean validInput;
+        int number, guess;
+        String answer;
+        
+        //Pick a random number 
+        number = getRandomNumber();
+        
+        //Get the guess
+        System.out.println("\nI'm thinking of a number between 1 and 10.");
+        System.out.print("What do you think it is? ");
+        guess = getGuess();
+        
+        //Check the guess
+        if(guess == number) 
+            System.out.println("You're right!");
+        else
+            System.out.println("You're wrong! The number was " + number);
+    }
+    
+    public static int getRandomNumber(){
+        return (int)(Math.random() * 10) + 1;
+    }
+    
+    public static int getGuess(){
+        while(true){ //only exits if the user enters an number between 1 and 10
+            int guess = sc.nextInt();
+            if((guess < 1) || (guess > 10)){
+                System.out.print("I said, between 1 and 10. Try again: ");
+            }
+            else 
+                return guess;
+        }
+    }
+    
+    public static boolean askForAnotherRound(){
+        while(true){
+            String answer; 
+            System.out.print("\nPlay again? (Y or N) ");
+            answer = sc.next();
+            
+            if(answer.equalsIgnoreCase("Y"))
+                
+                return true;
+            else if (answer.equalsIgnoreCase("N"))
+                return false;
+        }
     }
     
 }
