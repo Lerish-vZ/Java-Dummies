79c923996fb7364dc620a0b9b5cede48fc94aa36
Lerish-vZ <lerishavz@gmail.com>
2024-03-28 09:39:20 +0200

    update

---
+++ src/book2/chp8/GetInteger.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book2.chp8;
+
+/*
+TYPES OF EXCEPTIONS:
+    IllegalArgumentException - You passed an incorrect argument to a method.
+    InputMismatchException - The console input doesn't match the data type expected by a method of the Scanner class.
+    ArithmeticException - You tried an illegal type of arithmetic operation, such as dividing an interger by zero.
+    IOException - A method that performs I/O encountered an unrecoverable I/O error.
+    ClassNotFoundException - A necassary class couldn't be found.
+
+CATCH EXCEPTIONS USING A try STATEMENT: 
+SYNTAX: 
+    try{
+        statements that can throw exceptions
+        } catch (exception-type identifier) {
+        statements executed when exception is thrown
+        }
+        
+*/
+
+/**
+ *
+ * @author lerishav
+ */
+
+static 
+
+public class GetInteger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
