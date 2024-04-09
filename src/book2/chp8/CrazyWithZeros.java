1898c6eb2e43772ece0e8abe56c3b85b6785e371
Lerish-vZ <lerishavz@gmail.com>
2024-04-09 07:28:38 +0200

    update

--- src/book2/chp8/CrazyWithZeros.java
+++ src/book2/chp8/CrazyWithZeros.java
@@ -28,8 +28,15 @@ public class CrazyWithZeros {
         
         try {
             c = a / b;
-            
+            System.out.println("It worked!");
+        } catch (Exception e) {
+            System.out.println("Didn't work the first time.");
+            c = a / b;
+            System.out.println("It worked the second time!");
+        } finally {
+            System.out.println("Better clean up my mess");                    
         }
-    
+        System.out.println("It worked after all.");
+        return c;    
     }
 }
