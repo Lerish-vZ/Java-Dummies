ffa739b121b9836f56f6c0c90d3b3b85baed312f
Lerish-vZ <lerishavz@gmail.com>
2024-04-19 13:24:53 +0200

    update

--- src/book3/chp6/CloneTest.java
+++ src/book3/chp6/CloneTest.java
@@ -17,7 +17,12 @@ public class CloneTest {
        Employee emp1 = new Employee("Martinez", "Anthony");
        emp1.setSalary(40000.0);
        
+       Employee emp2 = (Employee)emp1.clone();
        
+       emp1.setLastName("Smith");
+       
+        System.out.println(emp1);
+        System.out.println(emp2);
     }
     
 }
