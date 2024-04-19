d9410078ece1ebaa233c3923c9f4890f50c08a43
Lerish-vZ <lerishavz@gmail.com>
2024-04-19 13:22:26 +0200

    update

---
+++ src/book3/chp6/CloneTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book3.chp6;
+
+/**
+ *
+ * @author lerishav
+ */
+public class CloneTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
