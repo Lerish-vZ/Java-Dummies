51558847734f6145753ac8aff741b53d53bcb538
Lerish-vZ <lerishavz@gmail.com>
2024-04-19 13:57:46 +0200

    update

--- src/book3/chp6/CloneTest2.java
+++ src/book3/chp6/CloneTest2.java
@@ -14,81 +14,126 @@ public class CloneTest2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Employee emp1 = new Employee("Martinez", "Anthony");
-       emp1.setSalary(40000.0);
-       
-       emp1.address = new Address("1300 N. First Street", "Fresno", "CA", "93702");
-       System.out.println("*** after cloning ***\n");
-       
-       Employee emp2 = (Employee)emp1.clone();
-       
-       printEmployee(emp1);
-       printEmployee(emp2);
-       
-       emp2.setLastName("Smith");
-       emp2.address = new Address("2503 N. 6th Street", "Fresno", "CA", "93722");
-       
-       System.out.println("*** after changing emp2 ***\n");
-       printEmployee(emp1);
-       printEmployee(emp2);
+        Employee emp1 = new Employee("Martinez", "Anthony");
+        emp1.setSalary(40000.0);
+
+        emp1.address = new Address("1300 N. First Street", "Fresno", "CA", "93702");
+        System.out.println("*** after cloning ***\n");
+
+        Employee emp2 = (Employee) emp1.clone();
+
+        printEmployee(emp1);
+        printEmployee(emp2);
+
+        emp2.setLastName("Smith");
+        emp2.address = new Address("2503 N. 6th Street", "Fresno", "CA", "93722");
+
+        System.out.println("*** after changing emp2 ***\n");
+        printEmployee(emp1);
+        printEmployee(emp2);
     }
-    
-    private static void printEmployee(Employee e){
+
+    private static void printEmployee(Employee e) {
         System.out.println(e.getFirstName() + " " + e.getLastName());
         System.out.println(e.address.getAddress());
         System.out.println("Salary: " + e.getSalary());
         System.out.println();
     }
-    
+
 }
 
+class Employee implements Cloneable {
 
-class Employee implements Cloneable{
-    private String lastName; 
-    private String firstName; 
+    private String lastName;
+    private String firstName;
     private Double salary;
     public Address address;
-    
-    public Employee(String lastName, String firstName){
+
+    public Employee(String lastName, String firstName) {
         this.lastName = lastName;
         this.firstName = firstName;
         this.address = new Address();
     }
-    
-    public String getLastName(){
+
+    public String getLastName() {
         return this.lastName;
     }
-    
-    public void setLastName(String lastName){
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
-    public String getFirstName(){
+
+    public String getFirstName() {
         return this.firstName;
     }
-    
-    public void setFirstName(String firstName){
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
-    public Double getSalary(){
+
+    public Double getSalary() {
         return this.salary;
     }
-    
-    public void setSalary(Double salary){
+
+    public void setSalary(Double salary) {
         this.salary = salary;
     }
-    
-    public Object clone(){
+
+    public Object clone() {
         Employee emp;
-        emp = new Employee(this.lastName, this.firstName);
-        emp.setSalary(this.salary);
+        try {
+            emp = (Employee) super.clone();
+            emp.address = (Address) address.clone();
+        } catch (CloneNotSupportedException e) {
+            return null; //will never happen
+        }
         return emp;
     }
-    
+
     public String toString() {
         return this.getClass().getName() + "[" + this.firstName + " " + this.lastName + ", " + this.salary + "]";
-                
+
     }
 }
-    
+
+class Address implements Cloneable {
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address() {
+        this.street = "";
+        this.city = "";
+        this.state = "";
+        this.zipCode = "";
+    }
+
+    public Address(String street, String city,
+            String state, String zipCode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public Object clone() 
+        →133
+{
+try {
+            return super.clone();
+         →137
+}
+catch (CloneNotSupportedException e) {
+            return null; // will never happen
+        }
+    }
+
+    public String getAddress() {
+        return this.street + "\n"
+                + this.city + ", "
+                + this.state + " "
+                + this.zipCode;
+    }
+}
