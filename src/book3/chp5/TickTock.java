bcde116a5e2d50568ba644a59d342c3a8bbcafb1
Lerish-vZ <lerishavz@gmail.com>
2024-04-22 12:08:34 +0200

    update

--- src/book3/chp5/TickTock.java
+++ src/book3/chp5/TickTock.java
@@ -20,6 +20,12 @@ public class TickTock {
     public static void main(String[] args) {
         // create a timer that calls the Ticker class
         //at one second intervals
+        
+        Timer t = new Timer(1000, new Ticker());
+        t.start();
+        
+        //display a message box to prevent the program from ending immediately 
+        JOptionPane.showMessageDialog(null, "Click OK to exit program");
     }
     
 }
