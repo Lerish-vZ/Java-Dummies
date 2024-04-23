72810589226e7b753113dd2b6663ea95e4fc773c
Lerish-vZ <lerishavz@gmail.com>
2024-04-23 10:09:39 +0200

    update

--- src/book3/chp7/TickTockStatic.java
+++ src/book3/chp7/TickTockStatic.java
@@ -22,4 +22,25 @@ public class TickTockStatic {
         t.go();
     }
     
-}
+    private void go(){
+        //create a timer that calls the Ticker class at one second intervals
+        Timer t = new Timer(1000, new Ticker());
+        t.start();
+        
+        //display a message box to prevent the program from ending immediately
+        JOptionPane.showMessageDialog(null, "Click OK to exit program.");
+        System.exit(0); //cllas the exit method of the System class, which immediately shuts down the Java Virtual Machine. It's not required but the timer will continue a few more times after you have pressed ok if you do not use this.
+    }
+    
+    static class Ticker implements ActionListener {
+        private boolean tick = true; 
+        
+        public void actionPerformed (ActionEvent event){
+            if (tick){
+                System.out.println(tickMessage); //accesss a field of the outer class.
+            } else {
+                System.out.println(tockMessage);
+            }
+            tick = !tick;
+        }
+    }
