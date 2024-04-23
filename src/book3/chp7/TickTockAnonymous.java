337a9b783c6b73cd94f9f8437e3249a5d199401c
Lerish-vZ <lerishavz@gmail.com>
2024-04-23 11:07:49 +0200

    update

--- src/book3/chp7/TickTockAnonymous.java
+++ src/book3/chp7/TickTockAnonymous.java
@@ -14,11 +14,21 @@ import javax.swing.*;
 
 public class TickTockAnonymous {
 
-    /**
-     * @param args the command line arguments
-     */
+    private String tickMessage = "Tick..."; //these messages are defined in the outer class (inner classes can access them)
+    private String tockMessage = "Tock...";
+    
     public static void main(String[] args) {
-        
+        TickTockAnonymous t = new TickTockAnonymous();
+        t.go();
     }
     
+     private void go(){
+        //create a timer that calls the Ticker class at one second intervals
+        Timer t = new Timer(1000, new Ticker());
+        t.start();
+        
+        //display a message box to prevent the program from ending immediately
+        JOptionPane.showMessageDialog(null, "Click OK to exit program.");
+        System.exit(0); //cllas the exit method of the System class, which immediately shuts down the Java Virtual Machine. It's not required but the timer will continue a few more times after you have pressed ok if you do not use this.
+    }
 }
