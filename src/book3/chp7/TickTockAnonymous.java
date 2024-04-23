9e38bfaa2b3ea047ccdc21f3f747bb4f799c2981
Lerish-vZ <lerishavz@gmail.com>
2024-04-23 11:11:51 +0200

    update

--- src/book3/chp7/TickTockAnonymous.java
+++ src/book3/chp7/TickTockAnonymous.java
@@ -8,7 +8,6 @@ package book3.chp7;
  *
  * @author lerishav
  */
-
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -16,15 +15,30 @@ public class TickTockAnonymous {
 
     private String tickMessage = "Tick..."; //these messages are defined in the outer class (inner classes can access them)
     private String tockMessage = "Tock...";
-    
+
     public static void main(String[] args) {
         TickTockAnonymous t = new TickTockAnonymous();
         t.go();
     }
-    
-     private void go(){
+
+    private void go() {
         //create a timer that calls the Ticker class at one second intervals
-        Timer t = new Timer(1000, new Ticker());
+        Timer t = new Timer(1000,
+                new ActionListener() {
+            private boolean tick = true;
+
+            public void actionPerformed(ActionEvent event) {
+                if (tick) {
+                    System.out.println(tickMessage);
+                } else {
+                    System.out.println(tockMessage);
+                }
+                tick = !tick;
+            }
+        });
         
+        t.start();
+        JOptionPane.showMessageDialog(null, "Click OK to exit program.");
+        System.exit(0);
     }
 }
