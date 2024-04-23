d27fa8241b34fbf9e6d2c9934c402c0c92f1d894
Lerish-vZ <lerishavz@gmail.com>
2024-04-23 09:58:30 +0200

    update

--- src/book3/chp7/TickTockInner.java
+++ src/book3/chp7/TickTockInner.java
@@ -30,5 +30,9 @@ public class TickTockInner {
         //create a timer that calls the Ticker class at one second intervals
         Timer t = new Timer(1000, new Ticker());
         t.start();
+        
+        //display a message box to prevent the program from ending immediately
+        JOptionPane.showMessageDialog(null, "Click OK to exit program.");
+        System.exit(0);
     }
 }
