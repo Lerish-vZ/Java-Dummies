f7d1fd3fb910a594a24825538331f91998340ddb
Lerish-vZ <lerishavz@gmail.com>
2024-04-25 11:51:13 +0200

    update

---
+++ src/book3/chp8/Employee.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book3.chp8; //in book package is: com.lowewriter.payroll;
+
+/** Represents an employee.
+ *
+ * @author lerishav
+ * @author www.book3chp8.com
+ * @version 1.5
+ * @since 1.0
+ */
+public class Employee {
+    
+    private String lastName;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
