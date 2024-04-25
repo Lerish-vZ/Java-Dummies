515e6ff450ac7bd3558594a056fcd0254f6908e7
Lerish-vZ <lerishavz@gmail.com>
2024-04-25 11:57:06 +0200

    update

--- src/book3/chp8/Employee.java
+++ src/book3/chp8/Employee.java
@@ -4,7 +4,8 @@
  */
 package book3.chp8; //in book package is: com.lowewriter.payroll;
 
-/** Represents an employee.
+/**
+ * Represents an employee.
  *
  * @author lerishav
  * @author www.book3chp8.com
@@ -12,16 +13,45 @@ package book3.chp8; //in book package is: com.lowewriter.payroll;
  * @since 1.0
  */
 public class Employee {
-    
+
     private String lastName;
-    private String firstName; 
+    private String firstName;
     private Double salary;
-   
-    /** Represents the employee's address
-    */
+
+    /**
+     * Represents the employee's address
+     */
+    public Address address;
+
+    /**
+     * Creates an employee with the specified name.
+     *
+     * @param lastName The employee's last name.
+     * @param fistName The employee's first name.
+     */
+    public Employee(String lasName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = address;
+    }
+
+    /**
+     * Gets the employee's last name.
+     *
+     * @return A string representing the employee's last name.
+     */
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    /**
+     * Sets the employee's last name.
+     *
+     * @param lastName A String containing the employee's last name.
+     */
     
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
