2b19fbbb914928b5673dc3b0605eab3cdf02bf1f
Lerish-vZ <lerishavz@gmail.com>
2024-04-26 10:47:41 +0200

    update

--- src/book3/chp8/Employee.java
+++ src/book3/chp8/Employee.java
@@ -86,9 +86,3 @@ public class Employee {
     }
 }
 
-public class Address implements Cloneable{
-    public String street; 
-    public String city; 
-    public String state; 
-    public String zipCode;
-}
