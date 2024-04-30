93639746387c470ab615b550da7e01d0e2b6479a
Lerish-vZ <lerishavz@gmail.com>
2024-04-30 11:03:06 +0200

    update

---
+++ src/book4/chp1/MarkVowels_example.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4.chp1;
+
+/**
+ *
+ * @author lerishav
+ */
+import java.util.Scanner;
+public class MarkVowels_example {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
