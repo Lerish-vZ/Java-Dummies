4491e3f5cdac2f6e10459263ecf8fa6afbd6461e
Lerish-vZ <lerishavz@gmail.com>
2024-04-30 11:06:36 +0200

    update

--- src/book4/chp1/MarkVowels_example.java
+++ src/book4/chp1/MarkVowels_example.java
@@ -9,26 +9,34 @@ package book4.chp1;
  * @author lerishav
  */
 import java.util.Scanner;
+
 public class MarkVowels_example {
 
     /**
      * @param args the command line arguments
      */
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.print("Enter a String: ");
         String s = sc.nextLine();
-        String originalString = s; 
-        
+        String originalString = s;
+
         int vowelCount = 0;
-        
-         for(int i = 0; i < s.length(); i++){
+
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if( (c == 'A') || (c == 'a') ||
-                    (c == 'E') || (c == 'e') ||
-                    (c == 'I') || (c == 'i') ||
-                    (c == 'O') || (c == 'o') ||
-                    (c == 'U') || (c == 'u') ) 
+            if ((c == 'A') || (c == 'a')
+                    || (c == 'E') || (c == 'e')
+                    || (c == 'I') || (c == 'i')
+                    || (c == 'O') || (c == 'o')
+                    || (c == 'U') || (c == 'u')) {
+
+                String front = s.substring(0, i);
+                String back = s.substring(i + 1);
+                s = front + "*" + back;
+            }
+        }
+
     }
-    
 }
