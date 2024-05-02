c364998c4f39c705aacdc84fdf2394f48c2e61c8
Lerish-vZ <lerishavz@gmail.com>
2024-05-02 12:12:10 +0200

    update

---
+++ src/book4/chp1/StringBuilderApp_example.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4.chp1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lerishav
+ */
+public class StringBuilderApp_example {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter a string: ");
+        
+        String s = sc.nextLine(); 
+        StringBuilder sb = new StringBuilder(s);
+        
+        int vowelCount = 0;
+        
+        for 
+                
+    }
+    
+}
