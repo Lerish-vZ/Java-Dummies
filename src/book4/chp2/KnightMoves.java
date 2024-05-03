66d3f673646d7adf08846349742c18acca63538b
Lerish-vZ <lerishavz@gmail.com>
2024-05-03 11:11:35 +0200

    update

---
+++ src/book4/chp2/KnightMoves.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4.chp2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lerishav
+ */
+public class KnightMoves {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
