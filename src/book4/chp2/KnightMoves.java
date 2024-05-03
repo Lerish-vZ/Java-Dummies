e9afe3216a60ded67fc6448410594703ffe0ab6d
Lerish-vZ <lerishavz@gmail.com>
2024-05-03 11:15:05 +0200

    update

--- src/book4/chp2/KnightMoves.java
+++ src/book4/chp2/KnightMoves.java
@@ -16,6 +16,17 @@ public class KnightMoves {
      * @param args the command line arguments
      */
     static Scanner sc = new Scanner(System.in);
+    
+    //the following static array represents the 8 possible moves a knight can make. This is an 8 x 2 array
+    static int[][] moves = {    {-2, +1}, 
+                                {-1, +2},
+                                {+1, +2},
+                                {+2, +1},
+                                {+2, -1},
+                                {+1, -2},
+                                {-1, -2},
+                                {-2, -1}    };
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
