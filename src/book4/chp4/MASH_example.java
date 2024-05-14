99dda79331bb0fbaafa68a2fb8b2b0ab6b6709fb
Lerish-vZ <lerishavz@gmail.com>
2024-05-14 10:49:24 +0200

    update

---
+++ src/book4/chp4/MASH_example.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4.chp4;
+
+import java.util.*;
+/**
+ *
+ * @author lerishav
+ */
+public class MASH_example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
