669d88a214404a4502a99962d01435b7067204b8
Lerish-vZ <lerishavz@gmail.com>
2024-05-14 10:44:40 +0200

    update

--- src/book4/chp3/ArrayList_examples.java
+++ src/book4/chp3/ArrayList_examples.java
@@ -5,6 +5,7 @@
 package book4.chp3;
 
 import java.util.*;
+
 /**
  *
  * @author lerishav
@@ -21,6 +22,47 @@ public class ArrayList_examples {
         nums.add("Three");
         nums.add("Four");
         nums.add(2, "Two and a half");
+
+        /*
+        nums looks as follows now: 
+            One 
+            Two 
+            Two and a half
+            Three 
+            Four
+         */
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.println(nums.get(i));
+        } //OR
+
+        System.out.println("");
+
+        //Easiest way to access all the elements: enhanced for loop
+        for (String s : nums) {
+            System.out.println(s);
+        }
+
+        for (String s : nums) {
+            int i = nums.indexOf(s);
+            System.out.println("Item " + i + ": " + s);
+        }
+
+        String s;
+        Iterator e = nums.iterator();
+        while (e.hasNext()) {
+            s = (String) e.next();
+            System.out.println(s);
+        }
+
+        nums.clear();
+        nums.add("One");
+        nums.add("Two");
+        nums.add("Three");
+        System.out.println(nums);
+        nums.set(0, "Uno");
+        nums.set(1, "Dos");
+        nums.set(2, "Tres");
+        System.out.println(nums);
     }
-    
+
 }
