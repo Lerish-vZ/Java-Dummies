036845869737d1b58dca7c9bce9a6720d85df69e
Lerish-vZ <lerishavz@gmail.com>
2024-05-14 10:38:01 +0200

    update

---
+++ src/book4/chp3/ArrayList_examples.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4.chp3;
+
+import java.util.*;
+/**
+ *
+ * @author lerishav
+ */
+public class ArrayList_examples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<String> nums = new ArrayList<String>();
+        nums.add
+    }
+    
+}
