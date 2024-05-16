1c96f0d81b2f51e3c9ce5d0387457d2b8523ff75
Lerish-vZ <lerishavz@gmail.com>
2024-05-16 09:57:11 +0200

    update

---
+++ src/book4/chp5/GenStackTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4.chp5;
+
+/**
+ *
+ * @author lerishav
+ */
+public class GenStackTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GenStack<String> gs = new GenStack<String>();
+        
+        System.out.println("Pushing four items onto the stack.");
+    }
+    
+}
