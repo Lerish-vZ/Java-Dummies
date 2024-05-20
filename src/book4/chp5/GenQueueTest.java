89a4a0dae75fc293446f78e8c244473c0e11f8ef
Lerish-vZ <lerishavz@gmail.com>
2024-05-20 10:00:53 +0200

    update

---
+++ src/book4/chp5/GenQueueTest.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4.chp5;
+
+/**
+ *
+ * @author lerishav
+ */
+public class GenQueueTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GenQueue<Employee> empList;
+empList = new GenQueue<Employee>();
+GenQueue<HourlyEmployee> hList;
+hList = new GenQueue<HourlyEmployee>();
+hList.enqueue(new HourlyEmployee(
+"Trump", "Donald"));
+hList.enqueue(new HourlyEmployee(
+"Gates", "Bill"));
+hList.enqueue(new HourlyEmployee(
+"Forbes", "Steve"));
+empList.addItems(hList);
+while (empList.hasItems())
+{
+Employee emp = empList.dequeue();
+System.out.println(emp.firstName
++ " " + emp.lastName);
+}
+}
+}
+class Employee
+{public String lastName;
+public String firstName;
+public Employee() {}
+public Employee(String last, String first)
+{
+this.lastName = last;
+this.firstName = first;
+}
+public String toString()
+{
+return firstName + " " + lastName;
+}
+}
+class HourlyEmployee extends Employee
+{
+public double hourlyRate;
+public HourlyEmployee(String last, String first)
+{
+super(last, first);
+}
+}
